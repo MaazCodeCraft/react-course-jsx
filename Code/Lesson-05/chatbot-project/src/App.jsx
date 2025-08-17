b571e55820a4acc634a045f6cc67dc8723982b45
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-08-17 13:55:04 +0500

    Move main.jsx, CSS, Images, and ChatInput

--- Code/Lesson-05/chatbot-project/src/App.jsx
+++ Code/Lesson-05/chatbot-project/src/App.jsx
@@ -1,79 +1,8 @@
 import { useState, useRef, useEffect } from "react";
-import { Chatbot } from "supersimpledev";
+import RobotProfileImg from "./assets/robot.png";
+import UserProfileImg from "./assets/user.png";
+import LoadingSpiner from "./assets/loading-spinner.gif";
 import "./App.css";
-
-function ChatInput({ chatMessages, setChatMessages }) {
-  const [inputText, setInputText] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-
-  function saveInputText(event) {
-    setInputText(event.target.value);
-  }
-
-  function handleKeyDown(event) {
-    if (event.key === "Enter") {
-      sendMessage();
-    } else if (event.key === "Escape") {
-      setInputText("");
-    }
-  }
-
-  async function sendMessage() {
-    if (isLoading || inputText === "") {
-      return;
-    }
-
-    setIsLoading(true);
-    setInputText("");
-
-    const newChatMessages = [
-      ...chatMessages,
-      {
-        message: inputText,
-        sender: "user",
-        id: crypto.randomUUID(),
-      },
-      {
-        message: <img src="loading-spinner.gif" className="loading-spinner" />,
-        sender: "robot",
-        id: crypto.randomUUID(),
-      },
-    ];
-
-    setChatMessages(newChatMessages);
-    setInputText("");
-
-    const response = await Chatbot.getResponseAsync(inputText);
-
-    setChatMessages([
-      // ...newChatMessages,
-      ...newChatMessages.slice(0, newChatMessages.length - 1),
-      {
-        message: response,
-        sender: "robot",
-        id: crypto.randomUUID(),
-      },
-    ]);
-
-    setIsLoading(false);
-  }
-
-  return (
-    <div className="chat-input-container">
-      <input
-        placeholder="Send a message to Chatbot"
-        size="30"
-        onChange={saveInputText}
-        value={inputText}
-        className="chat-input"
-        onKeyDown={handleKeyDown}
-      />
-      <button onClick={sendMessage} className="send-button">
-        Send
-      </button>
-    </div>
-  );
-}
 
 function ChatMessage({ message, sender }) {
   // const message = props.message;
@@ -96,11 +25,11 @@ function ChatMessage({ message, sender }) {
       className={sender === "user" ? "chat-message-user" : "chat-message-robot"}
     >
       {sender === "robot" && (
-        <img src="robot.png" className="chat-message-profile" />
+        <img src={RobotProfileImg} className="chat-message-profile" />
       )}
       <div className="chat-message-text">{message}</div>
       {sender === "user" && (
-        <img src="user.png" className="chat-message-profile" />
+        <img src={UserProfileImg} className="chat-message-profile" />
       )}
     </div>
   );
---
+++ Code/Lesson-05/chatbot-project/src/components/ChatInput.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import { Chatbot } from "supersimpledev";
+
+function ChatInput({ chatMessages, setChatMessages }) {
+  const [inputText, setInputText] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  function saveInputText(event) {
+    setInputText(event.target.value);
+  }
+
+  function handleKeyDown(event) {
+    if (event.key === "Enter") {
+      sendMessage();
+    } else if (event.key === "Escape") {
+      setInputText("");
+    }
+  }
+
+  async function sendMessage() {
+    if (isLoading || inputText === "") {
+      return;
+    }
+
+    setIsLoading(true);
+    setInputText("");
+
+    const newChatMessages = [
+      ...chatMessages,
+      {
+        message: inputText,
+        sender: "user",
+        id: crypto.randomUUID(),
+      },
+      {
+        message: <img src={LoadingSpiner} className="loading-spinner" />,
+        sender: "robot",
+        id: crypto.randomUUID(),
+      },
+    ];
+
+    setChatMessages(newChatMessages);
+    setInputText("");
+
+    const response = await Chatbot.getResponseAsync(inputText);
+
+    setChatMessages([
+      // ...newChatMessages,
+      ...newChatMessages.slice(0, newChatMessages.length - 1),
+      {
+        message: response,
+        sender: "robot",
+        id: crypto.randomUUID(),
+      },
+    ]);
+
+    setIsLoading(false);
+  }
+
+  return (
+    <div className="chat-input-container">
+      <input
+        placeholder="Send a message to Chatbot"
+        size="30"
+        onChange={saveInputText}
+        value={inputText}
+        className="chat-input"
+        onKeyDown={handleKeyDown}
+      />
+      <button onClick={sendMessage} className="send-button">
+        Send
+      </button>
+    </div>
+  );
+}
